abfd4028452f0db1b196b7bf342c993328b88e99
kentcacho090-netizen <kentcacho090@gmail.com>
2026-09-05 07:23:37 +0800

    Initialize demo stock for catalog

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import "./categorySeed";
 import "./catalogSeed";
 import "./imageSeed";
+import "./stockSeed";
 import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
