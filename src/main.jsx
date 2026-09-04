b9b36ab1f32232f241f18474a36afb19a591fcab
kentcacho090-netizen <kentcacho090@gmail.com>
2026-09-05 07:55:55 +0800

    Switch storefront to clean stable app implementation

--- src/main.jsx
+++ src/main.jsx
@@ -5,9 +5,7 @@ import "./stockSeed";
 import "./remoteCatalog";
 import React from "react";
 import { createRoot } from "react-dom/client";
-import App from "./App";
-import "./styles.css";
-import "./hero.css";
+import App from "./AppClean";
 
 createRoot(document.getElementById("root")).render(
   <React.StrictMode><App /></React.StrictMode>
