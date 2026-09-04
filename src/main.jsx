3ea6c740f4d179e7164d0228b12d7f4a19878d1e
kentcacho090-netizen <kentcacho090@gmail.com>
2026-09-05 07:15:35 +0800

    Simplify hero welcome section

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,7 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./styles.css";
+import "./hero.css";
 
 createRoot(document.getElementById("root")).render(
   <React.StrictMode><App /></React.StrictMode>
