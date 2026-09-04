23f6d033c611b86f66ce52f052d11a98d0663f84
kentcacho090-netizen <kentcacho090@gmail.com>
2026-09-05 07:01:39 +0800

    feat: load expanded grocery categories

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,9 @@
+import "./categorySeed";
 import React from "react";
 import { createRoot } from "react-dom/client";
-import CachoStore from "./CachoStore";
+import App from "./App";
 import "./styles.css";
-import "./admin.css";
-import "./excel.css";
 
 createRoot(document.getElementById("root")).render(
-  <React.StrictMode><CachoStore /></React.StrictMode>
+  <React.StrictMode><App /></React.StrictMode>
 );
