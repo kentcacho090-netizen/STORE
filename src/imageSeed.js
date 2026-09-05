604549f9ea25d96aeb98ab4310972f64bddcae74
kentcacho090-netizen <kentcacho090@gmail.com>
2026-09-05 12:17:52 +0800

    Fix and expand exact product photo mappings

--- src/imageSeed.js
+++ src/imageSeed.js
@@ -1,14 +1,14 @@
 const PRODUCTS_KEY = "cacho-store-products-v1";
 
 // Public product photos matched to product name + exact unit/size whenever possible.
-// Where an exact package photo was found, prefer it over any placeholder image.
+// Prefer real package photos; leave a product's existing image alone when no reliable exact match was found.
 const IMAGE_MAP = {
   "Lucky Me! Pancit Canton": "https://cdn.mafrservices.com/sys-master-root/hfe/hfa/14787587145758/595516_main.jpg",
   "Lucky Me! Pancit Canton Original": "https://shopmetro.ph/angeles-supermarket/wp-content/uploads/2024/05/SM102169577-1-7.jpg",
   "Lucky Me! Pancit Canton Sweet & Spicy": "https://shoplilimart.com/cdn/shop/files/LuckyMePancitCantonSweet_SpicyFlavor-2.12oz.webp?v=1722184768&width=1800",
   "Lucky Me! Pancit Canton Kalamansi": "https://snfood.gr/SNFOOD_media/2025/03/10041277_lm-pc-kalamansi-80g.png",
   "Lucky Me! Pancit Canton Sweet & Spicy Kasalo Pack__120g": "https://media.pickaroo.com/media/thumb/variant_photos/2024/4/18/haKKdFwKrZUceUMe7LGZbV_watermark_400.jpg",
-  "Lucky Me! Pancit Canton Kalamansi Kasalo Pack__120g": "https://www.luckyme.ph/products/lucky-me-pancit-canton-kalamansi---kasalo-kalamansi",
+  "Lucky Me! Pancit Canton Kalamansi Kasalo Pack__120g": "https://shopmetro.ph/basak-supermarket/wp-content/uploads/2024/05/SM103304168-1-6.jpg",
 
   "Coca-Cola": "https://www.promiselandmart.com/cdn/shop/files/COKE_BOTTLED_1.5L_1024x1024%402x.jpg?v=1710311027",
   "Coca-Cola Original Taste__1.5L": "https://www.promiselandmart.com/cdn/shop/files/COKE_BOTTLED_1.5L_1024x1024%402x.jpg?v=1710311027",
@@ -21,6 +21,7 @@ const IMAGE_MAP = {
   "Bear Brand Fortified": "https://ph-test-11.slatic.net/p/3fd1183ccb8af6ee918453085d1833a7.jpg",
   "Bear Brand Fortified Powdered Milk Drink__135g": "https://smmarkets.ph/media/catalog/product/2/0/20168848_1.png",
   "Bear Brand Fortified Powdered Milk Drink__680g": "https://cdn.quicksell.co/-OASkWMKEOQ2eQzr55ZU/products/-OBB5npuVC1daxfgJ2us.jpg",
+  "Alaska Powdered Milk Plain__300g": "https://ever.ph/cdn/shop/files/100000008841-Alaska-Fortified-Powdered-Milk-Drink-Original-300g-220321_d931f3e9-2b29-4ed9-af85-94ae42ca2979.jpg?v=1772341734",
   "Argentina Corned Beef__175g": "https://k2pharmacy.ph/cdn/shop/files/ArgentinaCornedBeef175g1-fotor-20240627154056_grande.jpg?v=1720416375",
   "Argentina Corned Beef__150g": "https://sdcglobalchoice.com/wp-content/uploads/2021/07/150g-argentina-corned-beef-1024x1024.jpg",
   "Argentina Corned Beef": "https://k2pharmacy.ph/cdn/shop/files/ArgentinaCornedBeef175g1-fotor-20240627154056_grande.jpg?v=1720416375",
@@ -45,7 +46,9 @@ const IMAGE_MAP = {
   "Datu Puti Soy Sauce": "https://www.dmc.com.ph/web/image/product.template/1318/image_1024?unique=87948d3",
   "Datu Puti Vinegar__385ml": "https://akabanebussan.com/product/datu-puti-vinegar-385ml/21810.jpg",
   "Silver Swan Soy Sauce__385ml": "https://shopmetro.ph/marketmarket-supermarket/wp-content/uploads/2021/03/SM6948405-1.jpg",
-  "Cheez Whiz Plain": "https://www.prime-mart.ph/cdn/shop/files/cheezwhiz-plain-210g.jpg?v=1",
+  "UFC Banana Ketchup__320g": "https://zbga.shopsuki.ph/cdn/shop/files/014285000068_1_1024x.jpg?v=1710383360",
+  "Lady's Choice Mayonnaise__220ml": "https://down-ph.img.susercontent.com/file/ph-11134207-7rasl-m26ghw2q5sjgea",
+  "Cheez Whiz Plain": "https://cdn.store-assets.com/s/377840/i/16829303.jpeg",
   "Safeguard Pure White Soap": "https://himopt.com.ua/image/cache/catalog/image/cache/catalog/products/1111111/413408175-700x700.webp",
   "Silka Soap Papaya Green": "https://medias.watsons.com.ph/publishing/WTCPH-10088249-front-zoom.jpg?version=1734003326",
   "Fita Crackers": "https://sukli.com/cdn/shop/files/M.Y.San-FitaCrackers-CheeseFlavor-10Packs-300G_1080x.jpg?v=1749668473",
@@ -55,7 +58,8 @@ const IMAGE_MAP = {
   "Nescafe Classic__25g": "https://www.malvarmarketing.com/sites/default/files/styles/product/public/products/4800361420594.png?itok=nYUT78vz",
   "Dutch Mill Yoghurt Drink Mixed Fruit__90ml": "https://www.promiselandmart.com/cdn/shop/products/MI_1024x1024%402x.jpg?v=1597461827",
   "Nagaraya Cracker Nuts__50g": "https://img06.weeecdn.com/item/image/725/275/B245D9CBB9893C8.jpg%21c750x0.jpeg",
-  "Oishi Prawn Crackers__90g": "https://nordictindahan.fi/cdn/shop/files/oishi-prawn-crackers-90g.png?v=1758618478"
+  "Oishi Prawn Crackers__90g": "https://nordictindahan.fi/cdn/shop/files/oishi-prawn-crackers-90g.png?v=1758618478",
+  "Milo Chocolate Drink__24g": "https://static.wixstatic.com/media/720a1c_f2da96d3a24547df91e7083db440137c~mv2.png/v1/fill/w_518%2Ch_607%2Cal_c%2Clg_1%2Cq_85%2Cenc_avif%2Cquality_auto/720a1c_f2da96d3a24547df91e7083db440137c~mv2.png"
 };
 
 try {
