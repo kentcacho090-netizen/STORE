47e87fbbef4916a791fde37c98455a25c9fc545b
kentcacho090-netizen <kentcacho090@gmail.com>
2026-09-05 08:53:39 +0800

    Make storefront customer-only

--- src/main.jsx
+++ src/main.jsx
@@ -5,11 +5,9 @@ import "./stockSeed";
 import "./checkoutFix";
 import React from "react";
 import { createRoot } from "react-dom/client";
-import App from "./AppClean";
+import App from "./CustomerApp";
 import "./product-polish.css";
-import "./orderBridge";
-import "./orderBridge.css";
-import "./storeTools";
+import "./customerOrders";
 
 createRoot(document.getElementById("root")).render(
   <React.StrictMode><App /></React.StrictMode>
