949d9c77a742271a04014b96e7ce3aee33d3ac17
kentcacho090-netizen <kentcacho090@gmail.com>
2026-09-05 08:13:40 +0800

    Use React-controlled quantity controls only

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,6 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./AppClean";
 import "./product-polish.css";
-import "./quantityButtonText.js";
 
 createRoot(document.getElementById("root")).render(
   <React.StrictMode><App /></React.StrictMode>
