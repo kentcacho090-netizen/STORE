8ce0e79b7d2c873beaa787c58bd26ace567fefc7
kentcacho090-netizen <kentcacho090@gmail.com>
2026-09-05 08:05:58 +0800

    Show selected quantity on Add to order buttons

--- src/main.jsx
+++ src/main.jsx
@@ -6,6 +6,7 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./AppClean";
 import "./product-polish.css";
+import "./quantityButtonText.js";
 
 createRoot(document.getElementById("root")).render(
   <React.StrictMode><App /></React.StrictMode>
