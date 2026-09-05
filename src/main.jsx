d11cea19a86537929ff2b399a5cec37c50eca354
kentcacho090-netizen <kentcacho090@gmail.com>
2026-09-05 11:40:13 +0800

    Load final customer UI polish

--- src/main.jsx
+++ src/main.jsx
@@ -9,6 +9,7 @@ import App from "./CustomerD1";
 import "./product-polish.css";
 import "./customerOrders.css";
 import "./customerOrders";
+import "./customer-final.css";
 
 createRoot(document.getElementById("root")).render(
   <React.StrictMode><App /></React.StrictMode>
