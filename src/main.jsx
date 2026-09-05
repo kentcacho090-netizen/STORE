c80fe537910f8ea2eb6f8ffbab9c0af87015efea
kentcacho090-netizen <kentcacho090@gmail.com>
2026-09-05 08:02:10 +0800

    Use polished storefront with improved product cards

--- src/main.jsx
+++ src/main.jsx
@@ -2,14 +2,11 @@ import "./categorySeed";
 import "./catalogSeed";
 import "./imageSeed";
 import "./stockSeed";
-import "./remoteCatalog";
 import React from "react";
 import { createRoot } from "react-dom/client";
-import CachoStore from "./CachoStore";
-import "./styles.css";
-import "./hero.css";
-import "./mobile.css";
+import App from "./AppClean";
+import "./product-polish.css";
 
 createRoot(document.getElementById("root")).render(
-  <React.StrictMode><CachoStore /></React.StrictMode>
+  <React.StrictMode><App /></React.StrictMode>
 );
