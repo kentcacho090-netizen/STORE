395045ccb1bc371b3eba02cf6e6e622e1866e282
kentcacho090-netizen <kentcacho090@gmail.com>
2026-09-05 08:37:20 +0800

    Enable wholesale checkout and order manager

--- src/main.jsx
+++ src/main.jsx
@@ -7,6 +7,8 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./AppClean";
 import "./product-polish.css";
+import "./orderBridge";
+import "./orderBridge.css";
 
 createRoot(document.getElementById("root")).render(
   <React.StrictMode><App /></React.StrictMode>
