a285d5a25129e2ba8b0e490034d59def1aacd489
kentcacho090-netizen <kentcacho090@gmail.com>
2026-09-05 11:51:10 +0800

    Enable customer category scroll controls

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import "./catalogSeed";
 import "./imageSeed";
 import "./stockSeed";
 import "./checkoutFix";
+import "./categoryScrollFix";
 import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./CustomerD1";
